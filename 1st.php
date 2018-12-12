dab6281c2d0100697d4ff343a04a9679da94618a
imeshaa <42459556+imeshaa@users.noreply.github.com>
2018-12-12 14:21:30 +0530

    Update 1st.php

--- 1st.php
+++ 1st.php
@@ -8,5 +8,5 @@ $default= ($_GET['lang']=='') ? 'en' : $_GET['lang'];
 	echo $lang[$default]['hello'].','.$lang[$default]['my_name'].' John Doe';
 
  ?>hhhhhhhhhhh
-
+asasaasasasasasasasasasasasasa
 
